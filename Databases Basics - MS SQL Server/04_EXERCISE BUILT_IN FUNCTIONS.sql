fc5a0e1beee2aa4db4064a163da83e5e05610b06
Dimitar Todorov <mitaka206@gmail.com>
2017-09-27 23:12:10 +0300

    ASDASD

--- Databases Basics - MS SQL Server/04_EXERCISE BUILT_IN FUNCTIONS.sql
+++ Databases Basics - MS SQL Server/04_EXERCISE BUILT_IN FUNCTIONS.sql
@@ -1,75 +1,36 @@
 
 --Part I – Queries for SoftUni Database
+
 --Problem 1.	Find Names of All Employees by First Name
---Write a SQL query to find first and last names of all employees whose first name starts with “SA”. Submit your query statements as Prepare DB & run queries.
---Example
---FirstName	LastName
---Sariya	Harnpadoungsataya
---Sandra	Reategui Alayo
---…	…
 USE SoftUni
 
 SELECT FirstName, LastName FROM Employees
 WHERE LEFT(FirstName, 2) = 'SA'
 
 
---Problem 2.	  Find Names of All employees by Last Name 
---Write a SQL query to find first and last names of all employees whose last name contains “ei”. Submit your query statements as Prepare DB & run queries.
---Example
---FirstName	LastName
---Kendall	Keil
---Christian	Kleinerman
---…	…
+--Problem 2.	  Find Names of All employees by Last Name
 SELECT FirstName, LastName FROM Employees
 WHERE LastName LIKE '%ei%'
 
 
 --Problem 3.	Find First Names of All Employees
---Write a SQL query to find the first names of all employees in the departments with ID 3 or 10 and whose hire year is between 1995 and 2005 inclusive. Submit your query statements as Prepare DB & run queries.
---Example
---FirstName
---Deborah
---Wendy
---Candy
---…
 SELECT * FROM Employees
 WHERE DepartmentId IN(3, 10) AND DepartmentId BETWEEN 1995 AND 2005
 ORDER BY HireDate
 
 
 --Problem 4.	Find All Employees Except Engineers
---Write a SQL query to find the first and last names of all employees whose job titles does not contain “engineer”. Submit your query statements as Prepare DB & run queries.
---Example
---FirstName	LastName
---Guy	Gilbert
---Kevin	Brown
---Rob	Walters
---…	…
 SELECT FirstName, LastName FROM Employees
 WHERE JobTitle NOT LIKE'%engineer%'
 
 
 --Problem 5.	Find Towns with Name Length
---Write a SQL query to find town names that are 5 or 6 symbols long and order them alphabetically by town name. Submit your query statements as Prepare DB & run queries.
---Example
---Name
---Berlin
---Duluth
---Duvall
---…
 SELECT Name FROM Towns
 WHERE LEN(Name) BETWEEN 5 AND 6
 ORDER BY Name
 
 
 --Problem 6.	 Find Towns Starting With
---Write a SQL query to find all towns that start with letters M, K, B or E. Order them alphabetically by town name. Submit your query statements as Prepare DB & run queries.
---Example
---TownID	Name
---5	Bellevue
---31	Berlin
---30	Bordeaux
---…	…
 SELECT * FROM Towns
 WHERE LEFT(Name, 1) LIKE '%M%'
    OR LEFT(Name, 1) LIKE '%K%' 
@@ -79,13 +40,6 @@ ORDER BY Name
 
 
 --Problem 7.	 Find Towns Not Starting With
---Write a SQL query to find all towns that does not start with letters R, B or D. Order them alphabetically by name. Submit your query statements as Prepare DB & run queries.
---Example
---TownID	Name
---2	Calgary
---23	Cambridge
---15	Carnation
---…	…
 SELECT * FROM Towns
 WHERE LEFT(Name, 1) NOT LIKE '%R%'
   AND LEFT(Name, 1) NOT LIKE '%B%'
@@ -94,13 +48,6 @@ ORDER BY Name
 
 
 --Problem 8.	Create View Employees Hired After 2000 Year
---Write a SQL query to create view V_EmployeesHiredAfter2000 with first and last name to all employees hired after 2000 year. Submit your query statements as Run skeleton, run queries & check DB.
---Example
---FirstName	LastName
---Steven	Selikoff
---Peter	Krebs
---Stuart	Munson
---...	...
 CREATE VIEW v_EmployeesHiredAfter2000 AS
 SELECT FirstName, LastName FROM Employees
 WHERE YEAR(HireDate) > 2000
@@ -109,14 +56,6 @@ SELECT * FROM v_EmployeesHiredAfter2000
 
 
 --Problem 9.	Length of Last Name
---Write a SQL query to find the names of all employees whose last name is exactly 5 characters long.
---Example
---FirstName	LastName
---Kevin	Brown
---Terri	Duffy
---Jo	Brown
---Diane	Glimp
---…	…
 SELECT FirstName, LastName FROM Employees
 WHERE LEN(LastName) = 5
 
@@ -124,12 +63,6 @@ WHERE LEN(LastName) = 5
 --Part II – Queries for Geography Database 
 
 --Problem 10.	Countries Holding ‘A’ 3 or More Times
---Find all countries that holds the letter 'A' in their name at least 3 times (case insensitively), sorted by ISO code. Display the country name and ISO code. Submit your query statements as Prepare DB & run queries.
---Example
---CountryName	ISO Code
---Afghanistan	AFG
---Albania	ALB
---…	…
 USE Geography
 
 SELECT CountryName, IsoCode FROM Countries
@@ -138,13 +71,6 @@ ORDER BY IsoCode
 
 
 --Problem 11.	 Mix of Peak and River Names
---Combine all peak names with all river names, so that the last letter of each peak name is the same like the first letter of its corresponding river name. Display the peak names, river names, and the obtained mix (mix should be in lowercase). Sort the results by the obtained mix. Submit your query statements as Prepare DB & run queries.
---Example
---PeakName	RiverName	Mix
---Aconcagua	Amazon	aconcaguamazon
---Aconcagua	Amur	aconcaguamur
---Banski Suhodol	Lena	banski suhodolena
---…	…	…
 SELECT PeakName , RiverName, LOWER( CONCAT(SUBSTRING(PeakName, 1, LEN(PeakName)-1), RiverName)) AS Mix
     FROM Peaks, Rivers
    WHERE (SUBSTRING(PeakName,  LEN(PeakName), LEN(PeakName)) = SUBSTRING(RiverName, 1,1))
@@ -154,13 +80,6 @@ ORDER BY Mix;
 --Part III – Queries for Diablo Database
 
 --Problem 12.	Games from 2011 and 2012 year
---Find the top 50 games ordered by start date, then by name of the game. Display only games from 2011 and 2012 year. Display start date in the format “yyyy-MM-dd”. Submit your query statements as Prepare DB & run queries.
---Example
---Name	Start
---Rose Royalty	2011-01-05
---London	2011-01-13
---Broadway	2011-01-16
---…	…
 use Diablo
 
 SELECT TOP (50) Name, FORMAT(Start,'yyyy-MM-dd') AS Start FROM Games
@@ -169,41 +88,17 @@ ORDER BY Start
 
 
 --Problem 13.	 User Email Providers
---Find all users along with information about their email providers. Display the username and email provider. Sort the results by email provider alphabetically, then by username. Submit your query statements as Prepare DB & run queries.
---Example
---Username	Email Provider
---Pesho	abv.bg
---monoxidecos	astonrasuna.com
---bashsassafras	balibless
---…	…
 SELECT Username, RIGHT(Email, LEN(Email) - CHARINDEX('@',Email)) AS 'Email Provider' FROM Users
 ORDER BY RIGHT(Email, LEN(Email) - CHARINDEX('@',Email)), Username ASC
 
 
 --Problem 14.	 Get Users with IPAdress Like Pattern
---Find all users along with their IP addresses sorted by username alphabetically. Display only rows that IP address matches the pattern: “***.1^.^.***”. Submit your query statements as Prepare DB & run queries.
---Legend: * - one symbol, ^ - one or more symbols
---Example
---Username	IP Address
---bindbawdy	192.157.20.222
---evolvingimportant	223.175.227.173
---inguinalself	255.111.250.207
---…	…
 SELECT Username, IpAddress FROM Users
 WHERE IpAddress LIKE '___.1%.%.___'
 ORDER BY Username
 
 
 --Problem 15.	 Show All Games with Duration and Part of the Day
---Find all games with part of the day and duration sorted by game name alphabetically then by duration (alphabetically, not by the timespan) and part of the day (all ascending). Parts of the day should be Morning (time is >= 0 and < 12), Afternoon (time is >= 12 and < 18), Evening (time is >= 18 and < 24). Duration should be Extra Short (smaller or equal to 3), Short (between 4 and 6 including), Long (greater than 6) and Extra Long (without duration). Submit your query statements as Prepare DB & run queries.
---Example
---Game	Part of the Day	Duration
---Ablajeck	Morning	Long
---Ablajeck	Afternoon	Short
---Abregado Rae	Afternoon	Long
---Abrion	Morning	Extra Short
---Acaeria	Evening	Long
---…	…	…
 USE Diablo
 
   SELECT Name,
@@ -224,39 +119,10 @@ ORDER BY Name
 --Part IV – Date Functions Queries
 
 --Problem 16.	 Orders Table
---You are given a table Orders(Id, ProductName, OrderDate) filled with data. Consider that the payment for that order must be accomplished within 3 days after the order date. Also the delivery date is up to 1 month. Write a query to show each product’s name, order date, pay and deliver due dates. Submit your query statements as Prepare DB & run queries.
---Original Table
---Id	ProductName	OrderDate
---1	Butter	2016-09-19 00:00:00.000
---2	Milk	2016-09-30 00:00:00.000
---3	Cheese	2016-09-04 00:00:00.000
---4	Bread	2015-12-20 00:00:00.000
---5	Tomatoes	2015-12-30 00:00:00.000
---…	…	…
---Output
---ProductName	OrderDate	Pay Due	Deliver Due
---Butter	2016-09-19 00:00:00.000	2016-09-22 00:00:00.000	2016-10-19 00:00:00.000
---Milk	2016-09-30 00:00:00.000	2016-10-03 00:00:00.000	2016-10-30 00:00:00.000
---Cheese	2016-09-04 00:00:00.000	2016-09-07 00:00:00.000	2016-10-04 00:00:00.000
---Bread	2015-12-20 00:00:00.000	2015-12-23 00:00:00.000	2016-01-20 00:00:00.000
---Tomatoes	2015-12-30 00:00:00.000	2016-01-02 00:00:00.000	2016-01-30 00:00:00.000
---…	…	…	…
+
+
+
 --Problem 17.	 People Table
---Create a table People(Id, Name, Birthdate). Write a query to find age in years, months, days and minutes for each person for the current time of executing the query.
---Original Table
---Id	Name	Birthdate
---1	Victor	2000-12-07 00:00:00.000
---2	Steven	1992-09-10 00:00:00.000
---3	Stephen	1910-09-19 00:00:00.000
---4	John	2010-01-06 00:00:00.000
---…	…	…
---Example Output
---Name	Age in Years	Age in Months	Age in Days	Age in Minutes
---Victor	16	189	5754	8286787
---Steven	24	288	8764	12621187
---Stephen	106	1272	38706	55737667
---John	6	80	2437	3510307
---…	…	…	…	…
 
 
 --L-----------A-------BBB--------------
